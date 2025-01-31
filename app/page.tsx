9bb051c7e9683ce881fc76af385379ef2913f241
WakhidHasim <wakhidhasim2000@gmail.com>
2025-01-31 22:29:04 +0700

    chore: add background color

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import WorkExperience from '@/components/WorkExperience';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col md:gap-12 gap-8 md:p-24 p-10 relative overflow-hidden lg:w-7/12 sm:w-full mx-auto'>
+    <main className='flex min-h-screen flex-col md:gap-12 gap-8 md:p-24 p-10 relative overflow-hidden lg:w-7/12 sm:w-full mx-auto bg-black'>
       <div className='fixed inset-0 pointer-events-none light-mode-fade-bottom dark:fade-bottom z-10 bg-white/10 dark:bg-black/10' />
       <Header />
       <WorkExperience />
